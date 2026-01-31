34e507b5503e3d8dbc5b425c9c48d4bcc889a8f4
Ricardo Martins dos Reis <ricardomreis22@hotmail.com>
2026-01-31 02:09:05 +0000

    Update package-lock.json

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const prismaClientSingleton = () => {
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+  });
   return new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 };
