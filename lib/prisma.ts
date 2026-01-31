319b64363eb180b5f002fbbf54b8d134b5281a28
Ricardo Martins dos Reis <ricardomreis22@hotmail.com>
2026-01-31 02:28:13 +0000

    back to prisma 6

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,7 @@
-import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL!,
-  });
   return new PrismaClient({
-    adapter,
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 };
